78e513cecbd61e4f1a5385ce82708f4cc2cc8ccf
Henrique Melo <hacmelo@gmail.com>
2022-03-09 22:38:17 -0300

    starting project

---
+++ layouts/default.vue
@@ -0,0 +1,49 @@
+<template>
+  <v-app :class="darkMode ? 'dark-mode' : 'light-mode'">
+    <Navbar />
+    <v-main>
+      <Nuxt />
+    </v-main>
+    <Footer />
+  </v-app>
+</template>
+
+<script>
+import Navbar from "./navbar.vue";
+import Footer from "./footer.vue";
+
+export default {
+  name: "DefaultLayout",
+  components: {
+    Navbar,
+    Footer,
+  },
+  data() {
+    return {
+      darkMode: true,
+    };
+  },
+};
+</script>
+
+<style>
+:root {
+  --DarkModeBackground: hsl(207, 26%, 17%);
+  --DarkModeElements: hsl(209, 23%, 22%);
+  --DarkModeText: hsl(0, 0%, 100%);
+  --LightModeBackground: hsl(0, 0%, 98%);
+  --LightModeElements: hsl(0, 0%, 100%);
+  --LightModeText: hsl(200, 15%, 8%);
+  --LightModeInput: hsl(0, 0%, 52%);
+}
+
+.light-mode {
+  background-color: var(--LightModeBackground) !important;
+  color: var(--LightModeText) !important;
+}
+
+.dark-mode {
+  background-color: var(--DarkModeBackground) !important;
+  color: var(--DarkModeText) !important;
+}
+</style>
---
+++ layouts/navbar.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-app-bar class="navbar" height="70">
+    <h1>Where in the world?</h1>
+    <v-switch v-model="darkMode" @click="switchMode()">Dark mode</v-switch>
+  </v-app-bar>
+</template>
+<script>
+export default {
+  name: "NavbarLayout",
+  data() {
+    return {
+      darkMode: false,
+    }
+  },
+  methods:{
+    switchMode() {
+      console.log(this.darkMode)
+    }
+  }
+};
+</script>
+<style scoped>
+.navbar {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 0 3%;
+}
+</style>
